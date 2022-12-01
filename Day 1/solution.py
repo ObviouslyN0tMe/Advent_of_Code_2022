11afc3b7168ad67f2145570324722429d10702cf
Josua <josua@jeuck.de>
2022-12-01 12:11:06 +0100

    Solved day 1

---
+++ Day 1/solution.py
@@ -0,0 +1,28 @@
+# puzzle input
+with open("input") as file:
+    rawdata = [x.strip("\n") for x in file.readlines()] + [""]
+
+carried_calories = []
+calories = 0
+elf = 0
+
+for line in rawdata:
+    if line:
+        print(line)
+        calories += int(line)
+    else:
+        print(line)
+        elf += 1
+        carried_calories.append((calories, "#" + str(elf)))
+        calories = 0
+
+
+carried_calories.sort()
+highest_calories = carried_calories[-1][0]
+total_three_highest_calories = carried_calories[-1][0] + carried_calories[-2][0] + carried_calories[-3][0]
+elfs_to_ask = carried_calories[-1][1] + ", " + carried_calories[-2][1] + " and " + carried_calories[-3][1]
+
+print("Elf to ask is", str(carried_calories[-1][1]), "and he carries", str(highest_calories), "calories.")
+
+print("The 3 Elfs to ask are ", elfs_to_ask + ". Together they carry", str(total_three_highest_calories), "calories.")
+
