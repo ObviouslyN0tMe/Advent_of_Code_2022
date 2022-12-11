b048b66c8568fd0eee3c49f441cd65d1a1882bb9
Josua <josua@jeuck.de>
2022-12-11 22:14:21 +0100

    solved day 9

--- Day 9/solution.py
+++ Day 9/solution.py
@@ -1,5 +1,5 @@
 # puzzle input
-with open("testinput") as file:
+with open("input") as file:
     rawdata = [x.strip("\n") for x in file.readlines()]
 
 
@@ -18,28 +18,33 @@ class Coordinate:
 def adjustTail(head, tail):
     diff_x = head.x - tail.x
     diff_y = head.y - tail.y
-    if abs(diff_x) == 2:
-        return Coordinate(head.x + diff_x/2, head.y)
+    if abs(diff_x) == abs(diff_y) == 2:
+        return Coordinate(head.x - diff_x//2, head.y - diff_y//2)
+    elif abs(diff_x) == 2:
+        return Coordinate(head.x - diff_x//2, head.y)
     elif abs(diff_y) == 2:
-        return Coordinate(head.y, head.y + diff_y/2)
+        return Coordinate(head.x, head.y - diff_y//2)
     else:
-        print(tail.x, tail.y)
         return tail
 
 
 movement_dict = {"L": Coordinate(-1, 0), "R": Coordinate(1, 0), "U": Coordinate(0, 1), "D": Coordinate(0, -1)}
 
-head_coord = Coordinate(0, 0)
-tail_coord = Coordinate(0, 0)
+knots = []
+for i in range(10):
+    knots.append(Coordinate(0, 0))
 
-tail_positions = {(0, 0): True}
+tail_positions_part1 = {(0, 0): True}
+tail_positions_part2 = {(0, 0): True}
 
 for line in rawdata:
     move = line.split(" ")
     for i in range(int(move[1])):
-        head_coord += movement_dict[move[0]]
-        tail_coord = adjustTail(head_coord, tail_coord)
-        tail_positions[(tail_coord.x, tail_coord.y)] = True
+        knots[0] += movement_dict[move[0]]
+        for knot in range(1, len(knots)):
+            knots[knot] = adjustTail(knots[knot - 1], knots[knot])
+        tail_positions_part1[(knots[1].x, knots[1].y)] = True
+        tail_positions_part2[(knots[-1].x, knots[-1].y)] = True
 
-print(tail_positions)
-print(len(tail_positions))
+print("Part 1", len(tail_positions_part1))
+print("Part 2", len(tail_positions_part2))
